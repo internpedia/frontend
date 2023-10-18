afa0259ac48f6b1b5dc50752cb42820a67c3879c
siasamaa <145623700+siasamaa@users.noreply.github.com>
2023-10-18 15:36:39 -0700

    Add files via upload

---
+++ src/components/Homepage.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="App.css"/>
+</head>
+<body>
+    <div class="container">
+        <div class="box">Review</div>
+        <div class="box">Review</div>
+        <div class="box">Review</div>
+        <div class="box">Review</div>
+        <div class="box">Review</div>
+    </div>
+</body>
+</html>
+      </header>
+    </div>
+  );
+}
+
+export default App;
