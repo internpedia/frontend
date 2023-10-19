a6bb9afad44136be4503638145f04f774eec41ff
Sia Sama <siasama01@gmail.com>
2023-10-18 20:10:19 -0700

    almost finished with card component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Homepage from './components/Homepage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="App.css"/>
-</head>
-<body>
-    <div class="container">
-        <div class="box">Review</div>
-        <div class="box">Review</div>
-        <div class="box">Review</div>
-        <div class="box">Review</div>
-        <div class="box">Review</div>
-    </div>
-</body>
-</html>
-      </header>
-    </div>
+    <Homepage/>
   );
 }
 
