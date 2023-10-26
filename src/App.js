22f46b298f2f19eaef4dd09604904111fe6d7f26
Michael Lu <michael.lu@berkeley.edu>
2023-10-25 21:03:16 -0700

    made progress on header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import "./App.css";
 import React from "react";
 import Homepage from "./pages/Homepage";
+import Header from "./components/Header"
+// import About from "./pages/About";
+import Contact from "./pages/Contact";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+
 
 function App() {
-  return <Homepage />;
+  return (
+    <>
+    <Header />
+    <main>
+    {/* <>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </> */}
+  <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          {/* <Route path="/about" element={<About/>} /> */}
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+    </BrowserRouter>
+    </main>
+  </>);
 }
 
 export default App;
