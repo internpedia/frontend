e00d19f4902dc15b47d5b93290cc8f5e0e7eb436
Christopher Bannon <bannon.c.35@berkeley.edu>
2023-11-01 20:52:24 -0700

    search result mid-fidelity

---
+++ src/pages/SearchPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+//Some changes made here
+
+function SearchPage() {
+  return <div>SearchPage</div>;
+}
+
+export default SearchPage;
--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,10 +1,15 @@
 import React from "react";
+import SearchResult from "../components/SearchResult";
 // IMPORT COMPONENTS HERE
 
 //Some changes made here
 
 function Test() {
-  return <div>PLACE YOUR COMPONENT/PAGE HERE!</div>;
+  return (
+    <div>
+      PLACE YOUR COMPONENT/PAGE HERE! <SearchResult />
+    </div>
+  );
 }
 
 export default Test;
