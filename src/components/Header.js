787c759b0e0d8ec6cc73d8c066981218354d5204
Christopher Bannon <bannon.c.35@berkeley.edu>
2023-11-08 20:57:16 -0800

    responsiveee

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,29 @@
-import React from 'react'
-import "./Header.css"
-import { rich_black, white} from '../colors';
-import { Link, BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import "./Header.css";
+import { rich_black, white } from "../colors";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="headerContainer" style={{backgroundColor: rich_black, color: white}}>
-        <div className='inlineContent'>
-            <div>Pfp</div>
-            <div style={{marginLeft: '24px'}}>INTERNPEDIA</div>
+    <div
+      className="headerContainer"
+      style={{ backgroundColor: rich_black, color: white }}
+    >
+      <div className="inlineContent">
+        <div class="logo">Pfp</div>
+        <div class="internpedia" style={{ marginLeft: "24px" }}>
+          INTERNPEDIA
         </div>
-        <div className='inlineContent'>
-            <Router>
-                <Link to="/">Write a Review</Link>
-                <Link to="/" style={{marginLeft: '15px', marginRight:'15px'}}>Contact Us</Link>
-                <Link to="/">Profile</Link>
-            </Router>
-        </div>
+      </div>
+      <div className="inlineContent">
+        <Router>
+          <Link to="/">Write a Review</Link>
+          <Link to="/" style={{ marginLeft: "15px", marginRight: "15px" }}>
+            Contact Us
+          </Link>
+          <Link to="/">Profile</Link>
+        </Router>
+      </div>
     </div>
   );
 }
