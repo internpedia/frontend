e4f70ff7e72ebf266c5a465b82b5f28aec3627c4
Nhu Vu <geniuslab00@gmail.com>
2023-11-29 16:48:07 -0800

    imported more fonts, almost done with search page

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,40 +1,46 @@
 import React from "react";
+import "./SearchResult.css";
+import pointIcon from './images/Vector 2.png';
+import StarRating from "./StarRating";
 
 //Some changes made here
 
-function SearchResult() {
+function SearchResult({data}) {
+
+  const { title:internshipName, company, rating, paid, location } = data;
+  
+
   return (
-    <div
+    <div className="listingBox"
       style={{
-        width: "200px",
-        height: "200px",
-        border: "1px solid black",
+        width: "307px",
+        height: "350px",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-      }}
-    >
-      <div style={{ width: "40px", height: "40px", backgroundColor: "grey" }}>
-        logo
+        flexWrap: "wrap",
+        justifyContent:"center",
+        gap:"3%",
+      }}>
+      <div style={{display: "flex", flexDirection:"column", alignItems:"center", gap: "6px"}}>
+        <img src={company.logo} alt={company.title} ></img>
+        <h1 style={{ fontSize: "15px", margin: "0px", padding: "0px" }}>{internshipName}</h1>
+        <h2 style={{ fontSize: "12px", margin: "0px", padding: "0px" }}>
+          {company.title}
+        </h2>
       </div>
-      <h1 style={{ fontSize: "16px", margin: "0px", padding: "0px" }}>Title</h1>
-      <h2 style={{ fontSize: "12px", margin: "0px", padding: "0px" }}>
-        Subtitle
-      </h2>
-      <div style={{ width: "60%", height: "20px", backgroundColor: "grey" }}>
-        Stars
+      <div style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"5px"}}>
+        <StarRating rating={rating}/> <p style={{fontFamily:"Roboto-Regular", fontSize:"14px"}}>{rating}</p>
       </div>
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <div>Thing 1</div>
-        <div>THing 2</div>
-      </div>
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <div>Thing 3</div>
-        <div>THing 4</div>
-      </div>
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <div>Thing 5</div>
-        <div>THing 6</div>
+      <div style={{display:"flex", flexDirection:"column", gap: "17px"}}>
+        <div className='supRow' style={{ display: "flex", flexDirection: "row" }}>
+          <img src={pointIcon}></img>
+          <div className='text'>{location}</div>
+        </div>
+        <div className='supRow' style={{ display: "flex", flexDirection: "row" }}>
+          <img src={pointIcon}></img>
+          <div className='text'>{paid ? "Paid":"Unpaid"}</div>
+        </div>
       </div>
     </div>
   );
